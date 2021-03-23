2080522bf1e6ac62a3b0679b9f7bd0432554c98e
Elton Zheng <elton.zheng.sh@gmail.com>
2021-03-23 16:50:34 +0800

    add next-seo support.

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,21 @@
 import '../styles/globals.css';
 import { Provider } from 'next-auth/client';
+import { DefaultSeo } from 'next-seo';
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider>
+      <DefaultSeo
+        openGraph={{
+          type: 'website',
+          locale: 'en_US',
+          url: `${process.env.NEXT_PUBLIC_ROOT_URL}`,
+          site_name: 'My Blog App',
+        }}
+        twitter={{
+          cardType: 'summary_large_image',
+        }}
+      />
       <Component {...pageProps} />
     </Provider>
   );
